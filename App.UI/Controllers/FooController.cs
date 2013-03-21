d67a09b4d5903d8b5b78a81d7102644f0e6b878f
pdrm6000 <pdrm6000@gmail.com>
2013-03-21 23:34:45 +0100

    test foo controller

---
+++ App.UI/Controllers/FooController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using App.Domain.AppServiceContracts;
+
+namespace App.UI.Controllers
+{
+    public class FooController : Controller
+    {
+        private readonly IArtistCollectorAppService _artistCollectorAppService;
+
+        public FooController(IArtistCollectorAppService artistCollectorAppService)
+        {
+            _artistCollectorAppService = artistCollectorAppService;
+        }
+
+        //
+        // GET: /Foo/
+
+        public ActionResult Index()
+        {
+            var result = _artistCollectorAppService.GetAll().ToList();
+            return View(result);
+        }
+    }
+}
