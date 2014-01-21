4c48d61ff97a4ac6b12f77c1251eb80b61e3f805
pdrm6000 <pdrm6000@gmail.com>
2014-01-21 17:08:50 +0100

    Comments section working with binding and popover

--- App.WebApp/App/viewmodels/review/comments.js
+++ App.WebApp/App/viewmodels/review/comments.js
@@ -8,15 +8,25 @@
 		var viewmodel = function () {
 			var self = this;
 			self.model = null;
+			
+			self.closePopover = function (p) {
+				$('#comments' + p.model.albumId()).hide();
+			};
+
+			self.showPopover = function(p) {
+				$('#comments' + p.model.albumId()).show();
+			};
 		};
 
 		viewmodel.prototype.attached = function () {
-			$('#popover' + this.model.albumId()).popover({
-			    html: true,
-				content: function () {
-				    return $('#' + this.id.replace('popover', 'comments')).html();
-				}
-			});
+			//$('#popover' + this.model.albumId()).popover({
+			//    html: true,
+			//    content: function () {
+			//    	var htmlCode = $('#' + this.id.replace('popover', 'comments')).html();
+			//	    $('#' + this.id.replace('popover', 'comments')).empty();
+			//	    return htmlCode;
+			//	}
+			//});
 		};
 
 		viewmodel.prototype.activate = function (param) {
