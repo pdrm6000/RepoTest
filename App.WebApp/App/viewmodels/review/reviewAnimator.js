868533580f5fc062d0ee3674fb5fe0c4e64a1599
pdrm6000 <pdrm6000@gmail.com>
2014-01-28 19:58:16 +0100

    animation for next album button

--- App.WebApp/App/viewmodels/review/reviewAnimator.js
+++ App.WebApp/App/viewmodels/review/reviewAnimator.js
@@ -4,22 +4,29 @@
 	],
 	function ($) {
 
-		var viewmodel = function () {
-			var self = this;
-		};
+	    var viewmodel = function () {
+	        var self = this;
+	    };
 
-		viewmodel.prototype.showAlbums = function (albumIds) {
-			$("#Albums").show();
-			$.each(albumIds, function(key, value) {
-				$('album' + value).delay(key * 500).fadeIn({ duration: 500 });
-			});
-		};
-		
-		viewmodel.prototype.hideAlbums = function (albumIds) {
-			$("#Albums").hide();
-		};
+	    viewmodel.prototype.showAlbums = function (albumIds) {
+	        $.each(albumIds, function (key, value) {
+	            $('#album' + value).delay(key * 100).fadeIn({ duration: 300 });
+	        });
+	    };
+
+	    viewmodel.prototype.hideAlbums = function (albumIds, callback) {
+	        $.each(albumIds, function (key, value) {
+	            var options = null;
+	            if (albumIds.length - 1 == key) {
+	                options = { duration: 300, complete: callback };
+	            } else {
+	                options = { duration: 300 };
+	            }
+	            $('#album' + value).delay(key * 100).fadeOut(options);
+	        });
+	    };
 
 
-		return viewmodel;
+	    return viewmodel;
 
 	});
